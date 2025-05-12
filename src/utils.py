1aeaf5b0d3d650eec1551521eef004d8ed7a8862
mrrdl <mmradul38@gmail.com>
2025-05-12 17:52:21 +0530

    Model Training & r2 calc

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exceptions import CustomException
 
@@ -12,5 +13,18 @@ def save_object(file_path,obj):
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            r2 = r2_score(y_test, y_pred)
+            report[list(models.keys())[i]] = r2
+        return report
     except Exception as e:
         raise CustomException(e, sys)
